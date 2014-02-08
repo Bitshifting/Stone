b7d88e52899fb9c9c37cafa74ef932c7d8d9fe0f
Kenneth Siu <kksiu@wisc.edu>
2014-02-07 22:56:23 -0500

    Initial stone code

---
+++ Stone/Stone/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Stone
+//
+//  Created by Kenneth Siu on 2/7/14.
+//  Copyright (c) 2014 Kenneth Siu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
